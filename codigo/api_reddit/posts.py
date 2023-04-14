67cb089b7380845d71efce0937ee91b9a93dc53c
karenrivgi <karenrivgi@gmail.com>
2023-04-13 22:28:50 -0500

    Clases posts agregada

---
+++ codigo/api_reddit/posts.py
@@ -0,0 +1,19 @@
+class Post:
+    """Clase usada para representar los post extraidos mediante la API de reddit.
+
+        atributos:
+            - header (str): titulo del post
+            - body (str): cuerpo de texto del post
+            - image_path (str): path local de la imagen guardada al momento de descargar las imagenes 
+            - author (str): autor del post
+            - score (int): numero de upvotes del post
+            - subreddit (str): nombre del subreddit del cual se recuperó el post
+    """
+
+    def __init__(self, header, body, author, score, subreddit, image_path = None):
+        self.header = header
+        self.body = body
+        self.image_path = image_path
+        self.author = author
+        self.score = score
+        self.subreddit = subreddit
