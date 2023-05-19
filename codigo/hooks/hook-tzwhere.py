74fc8d1b6aa094897a2909dbe843c155780501d2
YamidCampo <ycampo@unal.edu.co>
2023-05-18 21:35:55 -0500

    cambio hook tz-where

--- codigo/hooks/hook-tzwhere.py
+++ codigo/hooks/hook-tzwhere.py
@@ -2,6 +2,6 @@
 
 from PyInstaller.utils.hooks import collect_data_files
 
-hiddenimports = ['tzwhere', 'tzwhere.tzwhere']
+hiddenimports = ['tzwhere']
 # Incluir los archivos de datos necesarios para tzwhere
-datas = collect_data_files('tzwhere') + collect_data_files('tzwhere.tzwhere')
+datas = collect_data_files('tzwhere')
